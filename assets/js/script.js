c2bd991a58a48bc0a21d0ced1ac572971f4b9947
Jacqueline Hodgson <jacqueline.e.hodgson@gmail.com>
2022-01-29 11:11:40 -0500

    attempts to fix getlocal storage issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,6 @@
-//fix localstorage.get issue;
-//add input field sortable function?
-
 let dateEl = $("#currentDay");
 let hourRowEl = $(".input-group");
 
-let nineEl = $("#block9 .input-group-text");
-let tenEl = $("#block10 .input-group-text");
-let elevenEl = $("#block11 .input-group-text");
-let twelveEl = $("#block12 .input-group-text");
-let thirteenEl = $("#block13 .input-group-text");
-let fourteenEl = $("#block14 .input-group-text");
-let fifteenEl = $("#block15 .input-group-text");
-let sixteenEl = $("#block16 .input-group-text");
-let seventeenEl = $("#block17 .input-group-text");
 let saveBtnEl = $(".saveBtn");
 
 
@@ -21,59 +9,26 @@ function checkTime() {
   let now = moment().hour();
 
   hourRowEl.each(function() {
-    let worktime = parseInt($(this).attr("id").split("block")[1]);
-    // if now is later than worktime;
+    let worktime = parseInt($(this).attr("id").split("hour")[1]);
     if (worktime < now) {
-      // add a "past" class to the element
       $(this).children("textarea").addClass("past");
-    // if now is the same as worktime
     } else if (worktime === now) {
-        // add a "present" class to the element
         $(this).children("textarea").addClass("present");
     } else {
-      // if now is earlier than worktime add a "future" class to the element
       $(this).children("textarea").addClass("future");
     }
   });
 }
 
-function getLocalStorage() {
-
-  hourRowEl.each(function() {
-//     let storedTask = JSON.parse(localStorage.getItem($(this).children("textarea").val()));
-    
-//     if (storedTask !== null) {
-//       $(this).children("textarea").val() = storedTask;
-//     }
-// $("#nine-block .input-group-text").val(localStorage.getItem("nine"));
-// $("#ten-block .input-group-text").val(localStorage.getItem("ten"));
-// $("#eleven-block .input-group-text").val(localStorage.getItem("eleven"));
-// $("#twelve-block .input-group-text").val(localStorage.getItem("twelve"));
-// $("#one-block .input-group-text").val(localStorage.getItem("one"));
-// $("#two-block .input-group-text").val(localStorage.getItem("two"));
-// $("#three-block .input-group-text").val(localStorage.getItem("three"));
-// $("#four-block .input-group-text").val(localStorage.getItem("four"));
-// $("#five-block .input-group-text").val(localStorage.getItem("five"));
-  });
-}
-
-//initilization function;
 $(document).ready(function() {
-  //setting today's date with moment.js;
+
   dateEl.text(moment().format('MMMM Do, YYYY'));
-    //console.log(dateEl.text(moment().format('MMMM Do, YYYY')));
-    // create handler event for ready function;
+
     saveBtnEl.on("click", function() {
-      //get user input text content;
       let input = $(this).siblings("textarea").val();
-      //console.log(input);
-      //get timeslot associated with user input text;
       let timeslot = $(this).parent().attr("id");
-      //console.log(timeblock);
-      //add user input and time to local storage;
       localStorage.setItem(timeslot, input);
       });
-    // call getlocalstorage function;
+
     checkTime();
-    getLocalStorage();
   });
